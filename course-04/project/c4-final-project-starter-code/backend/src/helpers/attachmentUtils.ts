1754d308675f7a9d193d380abf5ba6bf3e4751cf
Badamasi Aliu <Badamasi.link@gmail.com>
2025-05-19 16:24:00 +0100

    feat(attachmentUtils): made changes to the attachment file

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/attachmentUtils.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/attachmentUtils.ts
@@ -4,3 +4,35 @@ import * as AWSXRay from 'aws-xray-sdk'
 const XAWS = AWSXRay.captureAWS(AWS)
 
 // TODO: Implement the fileStogare logic
+
+import * as AWS from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+import { createLogger } from '../utils/logger'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+const logger = createLogger('AttachmentUtils')
+
+export class AttachmentUtils {
+  constructor(
+    private readonly s3 = new XAWS.S3({ signatureVersion: 'v4' }),
+    private readonly bucketName = process.env.ATTACHMENT_S3_BUCKET,
+    private readonly urlExpiration = process.env.SIGNED_URL_EXPIRATION || '300'
+  ) { }
+
+  getAttachmentUrl(todoId: string): string {
+    logger.info(`Getting attachment URL for todo ${todoId}`)
+    return `https://${this.bucketName}.s3.amazonaws.com/${todoId}`
+  }
+
+  getUploadUrl(todoId: string): string {
+    logger.info(`Generating upload URL for todo ${todoId}`)
+
+    const url = this.s3.getSignedUrl('putObject', {
+      Bucket: this.bucketName,
+      Key: todoId,
+      Expires: parseInt(this.urlExpiration)
+    })
+
+    return url
+  }
+}
