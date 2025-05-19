d8b443ea3b971580cc773f5eb02009897403de98
Badamasi Aliu <Badamasi.link@gmail.com>
2025-05-19 16:24:32 +0100

    feat(createTodo): made changes to the attachment file

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -6,18 +6,23 @@ import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { getUserId } from '../utils';
 import { createTodo } from '../../helpers/todos'
 
-export const handler = middy(
-  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const newTodo: CreateTodoRequest = JSON.parse(event.body)
-    // TODO: Implement creating a new TODO item
-    const userId = getUserId(event)
-    const todoItem = await createTodo(newTodo, userId)
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  // TODO: Implement creating a new TODO item
+  const userId = getUserId(event)
+  const todoItem = await createTodo(newTodo, userId)
 
-    return todoItem
-)
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      item: todoItem
+    })
+  };
 
-handler.use(
-  cors({
-    credentials: true
-  })
-)
+}
+
+
